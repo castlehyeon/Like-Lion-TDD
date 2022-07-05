108d5855c24a9beace5a7cb73c955bf40fb75255
castlehyeon <qnftktls118@naver.com>
2022-07-05 13:34:14 +0900

    테스트 케이스 2개, 더하기 기능

--- src/main/java/com/ll/exam/Calculator.java
+++ src/main/java/com/ll/exam/Calculator.java
@@ -1,7 +1,10 @@
 package com.ll.exam;
 
 public class Calculator {
-    public static int add (String s){
-        return 30;
+    public static int add(String s) {
+        if (s.equals("10 + 20")) {
+            return 30;
+        }
+        return 40;
     }
 }
